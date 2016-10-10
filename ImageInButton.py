88a86904be172eeb877f4f7e3e1c7ee17d2c3b04
Siladittya Manna <jimmyakasilu@users.noreply.github.com>
2016-10-10 17:15:46 +0530

    Delete ImageInButton.py

--- ImageInButton.py
+++
@@ -1,8 +0,0 @@
-import PIL.Image
-import PIL.ImageTk
-AlphaImagePath = "H:\PROJECT\AlphabetImages\\"
-Photo=[]
-AlphaStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-for alpha in AlphaStr:
-    pathalpha = AlphaImagePath + alpha + '.png'
-    Photo.append(PIL.ImageTk.PhotoImage(PIL.Image.open(pathalpha)))
